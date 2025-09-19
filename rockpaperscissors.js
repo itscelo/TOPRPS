285a3665c95d56356e0f1d60a0c5fc600f98661f
itscelo <merito.email@gmail.com>
2025-09-19 14:51:57 -0400

    Added ui and images

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,35 @@
+let rockChoice = document.querySelector("#rock");
+let paperChoice = document.querySelector("#paper");
+let scissorsChoice = document.querySelector("#scissors");
+let outputBox = document.querySelector("#output");
+let winnerBox = document.querySelector("#winner");
+let userScoreElement = document.querySelector("#humanScore");
+let compScoreElement = document.querySelector("#computerScore");
+function getComputerChoice() {
+  let computerChoices = ["Rock", "Paper", "Scissors"]; // possible choices
+  let compRandom = Math.floor(Math.random() * 3); // random location for compChoices array
+  let finalChoice = computerChoices[compRandom]; // set the random number array, selects comp option
+  return finalChoice.toLowerCase();
+}
+
+scissorsChoice.addEventListener("click", function (event) {
+  let userChoice = "scissors";
+  let computerChoice = getComputerChoice();
+  playRound(computerChoice, userChoice);
+});
+
+paperChoice.addEventListener("click", function (event) {
+  let userChoice = "paper";
+  let computerChoice = getComputerChoice();
+  playRound(computerChoice, userChoice);
+});
+
+rockChoice.addEventListener("click", function (event) {
+  let userChoice = "rock";
+  let computerChoice = getComputerChoice();
+  playRound(computerChoice, userChoice);
+});
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -8,53 +40,44 @@ function getComputerChoice() {
   return finalChoice.toLowerCase();
 }
 
-function getHumanChoice() {
-  let userAnswer = prompt(
-    `Please select from the following options:\n "rock", "paper", "scissors"`
-  );
-  return userAnswer.toLowerCase();
-}
-
 function playRound(computerFinalPick, userFinalPick) {
-  console.log(`The computer chose ${computerFinalPick}`);
+  outputBox.textContent = `The computer chose ${computerFinalPick}`;
   if (computerFinalPick === userFinalPick) {
-    console.log("It's a draw!!");
+    winnerBox.textContent = "It's a draw!!";
     humanScore++;
     computerScore++;
   } else if (computerFinalPick === "paper" && userFinalPick === "rock") {
-    console.log("Computer wins paper beats rock!");
+    winnerBox.textContent = `Computer wins paper beats rock!`;
     computerScore++;
   } else if (computerFinalPick === "scissors" && userFinalPick === "paper") {
-    console.log("Computer wins scissors beats paper");
+    winnerBox.textContent = "Computer wins scissors beats paper!";
     computerScore++;
   } else if (computerFinalPick === "rock" && userFinalPick === "scissors") {
-    console.log("Computer wins rock beats scissors");
+    winnerBox.textContent = "Computer wins rock beats scissors!";
     computerScore++;
   } else if (userFinalPick === "paper" && computerFinalPick === "rock") {
-    console.log("User wins paper beats rock!");
+    winnerBox.textContent = "User wins paper beats rock!";
     humanScore++;
   } else if (userFinalPick === "scissors" && computerFinalPick === "paper") {
-    console.log("User wins scissors beats paper");
+    winnerBox.textContent = "User wins scissors beats paper!";
     humanScore++;
   } else if (userFinalPick === "rock" && computerFinalPick === "scissors") {
-    console.log("User wins rock beats scissors");
+    winnerBox.textContent = "User wins rock beats scissors!";
     humanScore++;
   }
-}
-
-for (let i = 0; i < 5; i++) {
-  let computerPick = getComputerChoice();
-  let userPick = getHumanChoice();
-  playRound(computerPick, userPick);
-  console.log(
-    "Human Score: " + humanScore + "\n" + "Computer Score: " + computerScore
-  );
-}
-
-if (humanScore > computerScore) {
-  console.log("The human won :o");
-} else if (computerScore > humanScore) {
-  console.log("The computer wins! :o");
-} else {
-  console.log("The game is a draw!!");
+  userScoreElement.textContent = humanScore;
+  compScoreElement.textContent = computerScore;
+  if (humanScore === 5) {
+    winnerBox.textContent = "HUMAN WINS!!!!";
+    humanScore = 0;
+    computerScore = 0;
+    userScoreElement.textContent = humanScore;
+    compScoreElement.textContent = computerScore;
+  } else if (computerScore === 5) {
+    winnerBox.textContent = "COMPUTER WINS!!!!";
+    humanScore = 0;
+    computerScore = 0;
+    userScoreElement.textContent = humanScore;
+    compScoreElement.textContent = computerScore;
+  }
 }
